5bbf1bf0fe9689073207b6e441971f722039fc99
Stephen Osunwale <hostephen@hodessy.com>
2023-03-03 10:46:28 +0100

    modified:   assets/images/output-onlinepngtools-2.png 	modified:   parts/header.html 	modified:   patterns/header-light.php 	modified:   templates/full-width-page-transparent-header.html 	modified:   theme.json

--- patterns/header-light.php
+++ patterns/header-light.php
@@ -6,9 +6,9 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"position":{"type":"sticky","top":"0px"},"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|30","right":"20px","bottom":"var:preset|spacing|30","left":"20px"}}},"className":"is-style-hody-addon-transparent-header","layout":{"type":"constrained","justifyContent":"center"}} -->
-<div class="wp-block-group is-style-hody-addon-transparent-header" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--30);padding-right:20px;padding-bottom:var(--wp--preset--spacing--30);padding-left:20px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
-<div class="wp-block-group alignwide" style="padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:image {"id":8,"width":120,"height":56,"sizeSlug":"full","linkDestination":"custom"} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"top":"20px","right":"20px","bottom":"var:preset|spacing|30","left":"20px"}}},"className":"is-style-hody-addon-transparent-header","layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group is-style-hody-addon-transparent-header" style="margin-top:0;margin-bottom:0;padding-top:20px;padding-right:20px;padding-bottom:var(--wp--preset--spacing--30);padding-left:20px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"0","top":"0","right":"0","left":"0"}}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":8,"width":120,"height":56,"sizeSlug":"full","linkDestination":"custom"} -->
 <figure class="wp-block-image size-full is-resized"><a href="/"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/output-onlinepngtools-2.png" alt="" class="wp-image-8" width="120" height="56"/></a></figure>
 <!-- /wp:image -->
 
