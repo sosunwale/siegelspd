db008a09269c856d3583252ddf5f722a60dad5e7
Stephen Osunwale <hostephen@hodessy.com>
2023-03-03 10:22:14 +0100

    Added a couple of Block Variations & Patterns

---
+++ patterns/testimonial-left.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Testimonial(left)
+ * Slug: siegelspd/testimonial-left
+ * Categories: text
+ * Inserter: yes
+ */
+?>
+<!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"backgroundColor":"background","className":"service-page-quote-column is-style-hodyaddon-group-box-shadow","layout":{"type":"constrained","contentSize":"500px"}} -->
+<div class="wp-block-group service-page-quote-column is-style-hodyaddon-group-box-shadow has-background-background-color has-background"><!-- wp:image {"align":"center","id":869,"width":64,"height":64,"sizeSlug":"full","linkDestination":"none","className":"service-page-quote-icon"} -->
+<figure class="wp-block-image aligncenter size-full is-resized service-page-quote-icon"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/quote.png" alt="" class="wp-image-869" width="64" height="64"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size"><br>Donec ullamcorper nulla non metus auctor fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"20px"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center","justifyContent":"left","orientation":"horizontal"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":177,"width":88,"height":58,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"5.3%"}},"className":"is-style-rounded"} -->
+<figure class="wp-block-image size-large is-resized has-custom-border is-style-rounded"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/replace-me.jpg" alt="" class="wp-image-177" style="border-radius:5.3%" width="88" height="58"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong>John Gibson</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><em>Married on 06/01/2019.</em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:gallery {"linkTo":"none","style":{"spacing":{"blockGap":{"top":"10px","left":"10px"}}}} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":177,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/replace-me.jpg" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":12,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/replace-me.jpg" alt=""/></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
